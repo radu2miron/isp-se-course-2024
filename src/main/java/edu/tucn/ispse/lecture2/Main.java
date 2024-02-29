f0fbc48411965de6c2d8597f58a302923e9069fe
Radu Miron <radu.miron@aut.utcluj.ro>
2024-02-29 11:09:16 +0200

    Project setup
    Signed-off-by: Radu Miron <radu.miron@aut.utcluj.ro>

---
+++ src/main/java/edu/tucn/ispse/lecture2/Main.java
@@ -0,0 +1,20 @@
+package edu.tucn.ispse.lecture2;
+
+import java.util.Scanner;
+
+/**
+ * @author Radu Miron
+ * @version 1
+ * <p>
+ * Simple(st) example.
+ */
+public class Main {
+    public static void main(String[] args) { // the main method is the place where the execution of the application starts
+        Scanner scanner = new Scanner(System.in); // create a scanner used from reading from the console
+
+        System.out.println("Your name: "); // print "Your name: " message in the console
+        String name = scanner.nextLine(); // read the input from the console
+
+        System.out.printf("Hello %s!", name); // print greeting in the console
+    }
+}
